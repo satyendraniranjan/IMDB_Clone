833f7a2c8fdfb76b4ffa7f9eb894f3ed1863be9c
sat.n760@gmail.com <sat.n760@gmail.com>
2019-09-01 04:01:14 +0530

    Added movie slider

--- movie/models.py
+++ movie/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.utils import timezone
 # Create your models here.
 
 CATEGORY_CHOICE = (
@@ -28,6 +29,7 @@ class Movie(models.Model):
     title = models.CharField(max_length = 100)
     description = models.TextField(max_length=1000)
     image = models.ImageField(upload_to='movies')
+    banner = models.ImageField(upload_to='movies_banner')
     category = models.CharField(choices=CATEGORY_CHOICE, max_length=10)
     language = models.CharField(choices=LANGUAGE_CHOICE,max_length=10)
     status = models.CharField(choices=STATUS_CHOICE,max_length=2)
@@ -35,6 +37,7 @@ class Movie(models.Model):
     year_of_production = models.DateField()
     view_count = models.IntegerField(default=0)
     movie_trailer = models.URLField()
+    created = models.DateTimeField(default=timezone.now)
 
     slug = models.SlugField(blank=True, null=True)
 

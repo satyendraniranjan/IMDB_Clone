d1f3ac47bd91dea3e3693f42b78e53ae041df4e8
sat.n760@gmail.com <sat.n760@gmail.com>
2019-09-02 22:36:00 +0530

    Added Home Page

--- movie/views.py
+++ movie/views.py
@@ -4,6 +4,19 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.dates import YearArchiveView
 # Create your views here.
 
+
+class HomeView(ListView):
+    model = Movie
+    template_name = 'movie/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['top_rated'] = Movie.objects.filter(status='TR')
+        context['most_watched'] = Movie.objects.filter(status='MW')
+        context['recently_added'] = Movie.objects.filter(status='RA')
+        return context
+
+
 class MovieList(ListView):
     model = Movie
     paginate_by = 2
